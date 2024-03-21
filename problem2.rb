f55211dcfb181d4b08d942a4581dec067ab1ca35
hrizeq17 <hrizeq@memphis.edu>
2024-03-21 14:59:44 -0500

    second  prob done

--- problem2.rb
+++ problem2.rb
@@ -1,17 +1,22 @@
 def parse_invoices(invoice_data)
     pattern = /^(\d{4}-\d{2}-\d{2}) - ([\w]+) - ([\w\s]+) - (\$\d+)/
+    puts "Starting invoice parsing..."
     invoice_data.each_line do |line|
-      if match = line.match(pattern)
-        puts "Date: #{match[1]}, Invoice Number: #{match[2]}, Client: #{match[3]}, Amount: #{match[4]}"
-      end
+      puts "Processing line: #{line.strip}"
+      match = line.match(pattern)
+    if match
+      puts "Date: #{match[1]}, Invoice Number: #{match[2]}, Client: #{match[3]}, Amount: #{match[4]}"
+    else
+      puts "No match found for line: #{line.strip}"
     end
   end
-  
-  invoice_entries = <<-INVOICES
-  2023-03-01 - INV001 - Acme Corp - $1000
-  2023-03-02 - INV002 - Beta LLC - $2050
-  2023-03-03 - INV003 - Gamma Inc - $3500
-  INVOICES
-  
-  parse_invoices(invoice_entries)
-  
+  puts "Finished invoice parsing."
+end
+
+invoice_entries = <<-INVOICES
+2023-03-01 - INV001 - Acme Corp - $1000
+2023-03-02 - INV002 - Beta LLC - $2050
+2023-03-03 - INV003 - Gamma Inc - $3500
+INVOICES
+
+parse_invoices(invoice_entries)
